540b46df6203419848ccc5d964c8f3c01cd94eb1
vibha1019 <vibhamandayam08@gmail.com>
2025-02-07 10:42:25 -0800

    add

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 # Allowed servers for cross-origin resource sharing (CORS)
-cors = CORS(app, supports_credentials=True, origins=['http://localhost:4887', 'http://127.0.0.1:4887', 'https://nighthawkcoders.github.io'])
+cors = CORS(app, supports_credentials=True, origins=['http://localhost:4887', 'http://127.0.0.1:8206', 'https://holiday.stu.nighthawkcodingsociety.com'])
 
 # System Defaults
 app.config['ADMIN_USER'] = os.environ.get('ADMIN_USER') or 'admin'
